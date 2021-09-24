d451166dd774362436c66dd1821d26c6da9134e9
ryoluo <40686845+ryoluo@users.noreply.github.com>
2021-09-24 21:45:58 +0900

    update README.md

--- pages/blog/_id.vue
+++ pages/blog/_id.vue
@@ -44,17 +44,7 @@
 </template>
 <script>
 import marked from 'marked'
-import Prism from 'prismjs'
-import 'prismjs/components/prism-sql'
-import 'prismjs/components/prism-python'
-import 'prismjs/components/prism-go'
-import 'prismjs/components/prism-bash'
-import 'prismjs/components/prism-markup-templating'
-import 'prismjs/components/prism-php'
-import 'prismjs/components/prism-json'
-import 'prismjs/components/prism-css'
-import 'prismjs/components/prism-sass'
-import 'prismjs/components/prism-scss'
+import Prism from '@/plugins/prism'
 export default {
   async asyncData({ params, $http }) {
     const renderer = new marked.Renderer()
---
+++ plugins/prism.js
@@ -0,0 +1,14 @@
+import Prism from 'prismjs'
+import 'prismjs/components/prism-sql'
+import 'prismjs/components/prism-python'
+import 'prismjs/components/prism-go'
+import 'prismjs/components/prism-bash'
+import 'prismjs/components/prism-markup-templating'
+import 'prismjs/components/prism-php'
+import 'prismjs/components/prism-json'
+import 'prismjs/components/prism-css'
+import 'prismjs/components/prism-sass'
+import 'prismjs/components/prism-scss'
+import 'prismjs/components/prism-nginx'
+
+export default Prism
